675549eddd06985ceb8c997666de1c5c454a9308
usmanajibola <usmanajibolaabass@gmail.com>
2021-08-10 01:57:34 +0100

    first commit

---
+++ fooddelivery/urls.py
@@ -0,0 +1,36 @@
+"""fooddelivery URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from customers.views import Index, About, Order, OrderConfirmation, OrderPayConfirmation, Menu, MenuSearch
+from restaurant.views import OrderDetails
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
+    path('restaurant/', include('restaurant.urls')),
+    path('menu/', Menu.as_view(), name='menu'),
+    path('menu/search/', MenuSearch.as_view(), name='menu-search'),
+    path('order/', Order.as_view(), name='order'),
+    path('order/<int:pk>/', OrderDetails.as_view(), name='order-details'),
+    path('order-confirmation/<int:pk>/', OrderConfirmation.as_view(), name='order-confirmation'),
+    path('payment-confirmation/', OrderPayConfirmation.as_view(), name='payment-confirmation'),
+    path('accounts/', include('allauth.urls')),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
